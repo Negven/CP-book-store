2224dc25858ae384b6b2650a475014bfdbb318eb
Negven <35338243+Negven@users.noreply.github.com>
2024-04-25 09:43:12 +0200

    feat: create pptx

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:client/@main/main_init.dart';
 import 'package:client/env/env.dart';
 import 'package:client/env/env_dev.dart';
 import 'package:flutter/material.dart';
-
+import 'package:dcdg/dcdg.dart';
 void main() async {
 
   // Fully extracting release preferences for debug
